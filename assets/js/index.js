7e6fb119ef7d9e3023f2a69c894e9ed911ff5664
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-11-25 18:49:01 +0600

    hero section completed

---
+++ assets/js/index.js
@@ -0,0 +1,2 @@
+ // On scroll animation js
+ AOS.init();
--- index.php
+++ index.php
@@ -5,13 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href='assets/images/favicon.png'>
     <title>Elen Energy</title>
+    <!-- On Scroll animation link -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/index.css">
     
   </head>
   <body>
+    <!-- Navbar start 
+    --------------------------- -->
     <?php require 'navbar.php'?>
-    <h1>Hello, world!</h1>
+
+
+
+    <!-- Hero section start
+    ---------------------------->
+    <section class="hero home__hero">
+      <div class="container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="row">
+          <div class="col-12">
+            <h3>JUST ONE CALL</h3>
+            <h1><b>MOVE ANYTHING</b></h1>
+            <h4>ANYWHERE</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- On scroll animation js link -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
     <script src="assets/js/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
