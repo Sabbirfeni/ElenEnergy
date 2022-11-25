6db8eedc123c24d4bafa8a14977fcddb44f18934
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-11-25 16:52:52 +0600

    added to godaddy

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+// Sidebar 
+// ---------------
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
---
+++ navbar.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Elen Energy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+  </head>
+  <body>
+
+    <!-- Navbar section start 
+    ------------------------- -->
+
+    <header class="fixed-top boldFont" id="header">
+        
+        <nav id="navbar" class="header-area shadow">
+            
+            <div class="logo-container d-flex align-items-center">
+                <a href="index.html">
+                    Elen Energy
+                    <!-- <img class="logo" src="assets/images/logo-InfinityCollar-updated.png" alt=""> -->
+                </a>
+            </div>
+
+            <div class="nav-item-container">
+                <ul class="nav-items-ul">
+                    <li><a href="explore.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="blogs.html">Blog</a></li>
+                   </ul>
+                   <!-- <a href="shop.html" class="nav_button " >Shop</a> -->
+                <button class="open-btn" onclick="openNav()">☰</button>
+            </div>
+
+
+            <!--******************
+            (Sidebar for tablet and mobile) section Start
+            ********************* -->
+            <div id="mySidepanel" class="side-panel">
+
+                <a href="javascript:void(0)" class="close-btn semiBoldFont" onclick="closeNav()"><img src="assets/images/right-arrow.png" alt="" style="width: 20px; height: 20px;"></a>
+                <ul>
+                    <li><a href="explore.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="blogs.html">Blog</a></li>
+                </ul>
+
+                <!-- <a href="index.html"><img src="assets/images/shopping-cart.png" alt="" style="width: 25px;"></a>
+                <a href="index.html"><img src="assets/images/user.png" alt="" style="width: 25px;"></a> -->
+            </div>
+            <!--******************
+            (Sidebar for tablet and mobile) section end
+            ********************* -->
+
+        </nav>
+    </header>
+    <script src="assets/js/navbar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
+    
+    
+    
+    
+    
+    
+    
